59df994ed8fd6792ad2485a40d01666c7730d4d1
patrick alexandre <palexandre@live.com>
2022-07-21 20:48:39 -0300

    JogoZika

---
+++ JogoZika/Personagem.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JogoZika
+{
+    class Personagem
+    {
+        public string nome { get; set; }
+        public decimal exp { get; set; }
+        public int level { get; set; }
+        public string classe { get; set; }
+        public string linhagem { get; set; }
+        public string cidadeInicial { get; set; }
+        public string cidadeAtual { get; set; }
+
+        public Personagem() { }
+
+        public Personagem(string nome, decimal exp, int level, string classe, string linhagem, string cidadeInicial, string cidadeAtual)
+        {
+            this.nome = nome;
+            this.exp = exp;
+            this.level = level;
+            this.classe = classe;
+            this.linhagem = linhagem;
+            this.cidadeInicial = cidadeInicial;
+            this.cidadeAtual = cidadeAtual;
+        }
+
+    }
+
+}
+
--- JogoZika/Program.cs
+++ JogoZika/Program.cs
@@ -1,12 +1,45 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace JogoZika
 {
     class Program
     {
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!!!!!!!!!adwd!!!");
+            string nome = "Bigodezinho";
+            decimal exp = 5050.50m;
+            int level = 100;
+            string classe = "Ladino";
+            string linhagem = "Meio-Elfo";
+            string cidadeInicial = "Konoha";
+            string cidadeAtual = "Aldeia da areia";
+
+            List<Personagem> Personagem = new List<Personagem>();
+            Personagem personagem = new Personagem();
+
+            personagem.nome = nome;
+            personagem.exp= exp;
+            personagem.level= level;
+            personagem.classe = classe;
+            personagem.linhagem= linhagem;
+            personagem.cidadeInicial= cidadeInicial;
+            personagem.cidadeAtual = cidadeAtual;
+
+            MostrarPesonagem(personagem);
+            
+        }
+        public static void MostrarPesonagem(Personagem personagem)
+        {
+            Console.WriteLine("Nome: " + personagem.nome);
+            Console.WriteLine("Experiência: " + personagem.exp);
+            Console.WriteLine("Level: " + personagem.level);
+            Console.WriteLine("Classe: " + personagem.classe);
+            Console.WriteLine("Linhagem: " + personagem.linhagem);
+            Console.WriteLine("Cidade natal: " + personagem.cidadeInicial);
+            Console.WriteLine("Cidade atual: " + personagem.cidadeAtual);
         }
     }
 }
