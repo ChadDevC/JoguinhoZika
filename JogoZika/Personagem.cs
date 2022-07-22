9ee77d6245793822f1daf3dd3a29eda564db1ba1
luizinhohg <loizhg96@gmail.com>
2022-07-21 21:45:07 -0300

    upzaada

---
+++ JogoZika/Personagem.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JogoZika
+{
+    public class Personagem
+    {
+        public string Nome { get; set; }
+        public decimal Exp { get; set; }
+
+
+
+        public string Classe { get; set; }
+        public string Linhagem { get; set; }
+        public string CidadeNatal { get; set; }
+        public string CidadeAtual { get; set; }
+
+        public int Level
+        {
+            get
+            {
+                int nivel = 1;
+                if (Exp > 100) { nivel = 2;}
+                if (Exp > 200) { nivel = 3;}
+                if (Exp > 300) { nivel = 4;}
+
+
+                int level = nivel;
+                return level;
+
+            }
+
+        }
+
+        public Personagem() { }
+
+        public Personagem(
+        string Nome,
+        decimal Exp,
+        string Classe,
+        string Linhagem,
+        string CidadeNatal,
+        string CidadeAtual)
+        {
+            this.Nome = Nome;
+            this.Exp = Exp;
+            this.Classe = Classe;
+            this.Linhagem = Linhagem;
+            this.CidadeNatal = CidadeNatal;
+            this.CidadeAtual = CidadeAtual;
+        }
+    }
+}
--- JogoZika/Program.cs
+++ JogoZika/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace JogoZika
 {
@@ -6,7 +7,45 @@ namespace JogoZika
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!!!!!!!!!!!!!");
+
+            string nome = "Luiz";
+            decimal exp = 5050.500m;
+            string classe = "Mago";
+            string linhagem = "Humano";
+            string cidadeNatal = "Konoha";
+            string cidadeAtual = "Aldeia da areia";
+
+
+
+            List<Personagem> Personagens = new List<Personagem>();
+           
+
+            Personagem personagem = new Personagem(nome, exp, classe, linhagem, cidadeNatal, cidadeAtual);
+
+
+            //Personagem personagem = new Personagem();
+            //personagem.Nome = nome;
+            //personagem.Exp = exp;            
+            //personagem.Classe = classe;
+            //personagem.Linhagem = linhagem;
+            //personagem.CidadeNatal = cidadeNatal;
+            //personagem.CidadeAtual = cidadeAtual;
+
+            //Personagens.Add(personagem);
+
+            MostrarPesonagem(personagem);
+
+
+
+        }
+        public static void MostrarPesonagem(Personagem personagem)
+        {
+            Console.WriteLine("Nome: " + personagem.Nome);
+            Console.WriteLine("Experiência: " + personagem.Exp);
+            Console.WriteLine("Classe: " + personagem.Classe);
+            Console.WriteLine("Linhagem: " + personagem.Linhagem);
+            Console.WriteLine("Cidade natal: " + personagem.CidadeNatal);
+            Console.WriteLine("Cidade atual: " + personagem.CidadeAtual);
         }
     }
 }
